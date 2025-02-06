77838728bbb6329fd7bdf41e4743068285486be9
bsegers <briansegs@gmail.com>
2025-02-05 19:01:18 -0500

    db model working\!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,12 +1,41 @@
 import {
   int,
   text,
-  singlestoreTable,
+  index,
+  singlestoreTableCreator,
   bigint,
 } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const creatTable = singlestoreTableCreator(
+  (name) => `drive-tutorial_${name}`,
+);
+
+export const files = creatTable(
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = creatTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
